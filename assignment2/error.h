043708ee365456eeca0bb5ea3d137d3f2e9e1cd7
ZHDreamer <ZHDreamerqaq@gmail.com>
2022-10-29 17:05:44 +0800

    initial commit

---
+++ assignment2/error.h
@@ -0,0 +1,8 @@
+#ifndef ERROR_H
+#define ERROR_H
+
+#include <string>
+
+void errorPrint(const std::string& msg = "",
+                const std::string& leftDisp = "===Error== ");
+#endif  // ERROR_H
